e6222d3abe26e59035f3649822e31bd939d0480b
Leonardo Souza <leogsouza@gmail.com>
2016-11-29 11:38:36 -0200

    Addes second parameter

--- workshoppers/esnext-generation/makeCounter.js
+++ workshoppers/esnext-generation/makeCounter.js
@@ -1,9 +1,9 @@
-module.exports = function makeCounter(someObj) {
+module.exports = function makeCounter(someObj, maxNum = 10) {
   var num = 0;
   var done = false;
   someObj.next = function() {
     // complete me
-    if (num < 10) {
+    if (num < maxNum) {
       num++;
     } else {
       done = true;
