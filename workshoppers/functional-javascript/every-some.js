e0e4bc1170a98af55970ddf5c0726ac7fc30e6ad
Leonardo Souza <leogsouza@gmail.com>
2016-12-16 09:52:14 -0200

    Functional Javascript - Exercise 5 - Resolved

---
+++ workshoppers/functional-javascript/every-some.js
@@ -0,0 +1,11 @@
+function checkUsersValid(goodUsers) {
+  return function allUsersValid(submittedUsers) {
+    return submittedUsers.every(function(user) {
+      return goodUsers.some(function(goodUser) {
+        return goodUser.id === user.id;
+      })
+    })
+  }
+}
+
+module.exports = checkUsersValid;
